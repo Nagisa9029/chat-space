d90e721a57c5a7ea7c9a1dee87029568585f58ce
Nagisa9029 <tzr.schicksal@gmail.com>
2020-06-06 00:23:01 +0900

    Add GroupMenbers Ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def index
+    @users = User.search(params[:keyword], current_user.id)
     respond_to do |format|
       format.json
+    end
   end
 
   def edit
