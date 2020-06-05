30d97ab303c5487948f8adc962bc0b4dc61bc5e3
Nagisa9029 <tzr.schicksal@gmail.com>
2020-06-05 15:28:08 +0900

    Create Jbuilder

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html = `<div class="ChatField">
+                    <div class="ChatField__Info">
+                      <div class="ChatField__Info__Name">
+                        ${message.user_name}
+                      </div>
+                      <div class="ChatField__Info__CreatedAt">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="ChatField__Message">
+                      <p class="ChatField__Message__comment">
+                        ${message.comment}
+                      </p>
+                      <img class="ChatField__Message__Image" src="${message.image}">
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      let html = `<div class="ChatField">
+                    <div class="ChatField__Info">
+                      <div class="ChatField__Info__Name">
+                        ${message.user_name}
+                      </div>
+                      <div class="ChatField__Info__CreatedAt">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="ChatField__Message">
+                      <p class="ChatField__Message__comment">
+                        ${message.comment}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+
   $('.Form').on('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action');
     $.ajax({
@@ -10,6 +49,10 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      console.log(html);
     })
   })
 });
